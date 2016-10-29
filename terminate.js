a2d07d1eee07d59b900517e4748dd97d6859adf0
Andy Edwards <jedwards@fastmail.com>
2016-10-28 21:13:33 -0500

    rewrite to use polling

--- terminate.js
+++ terminate.js
@@ -3,20 +3,48 @@ var handlePsTreeCallback = require('./handlePsTreeCallback');
 
 /**
  * terminate is an ultra-simple way to kill all the node processes
- * by providing a process.pid. It finds all child processes and shuts
+ * by providing a process pid. It finds all child processes and shuts
  * them down too, so you don't have to worry about lingering processes.
  * @param {int} pid - the Process ID you want to terminate
- * @param {function} callback (optional) - if you want to know once the
+ * @param {string=SIGTERM} signal (optional) - the signal to kill the processes
+ * with.
+ * @param {object={}} options
+ * @param {number=500} options.pollInterval - interval to poll whether pid
+ * and all of the child pids have been killed.
+ * @param {number=5000} options.timeout - max time to wait for processes to
+ * exit before timing out and calling back with an error.
+ * @param {function=} callback (optional) - if you want to know once the
  * procesess have been terminated, supply a callback.
- *   @param {string} error - will be null if no error occured
- *   @param {boolean} done - will be true if the operation succeeded.
+ *   @param {Error} error - will be null if no error occured
  */
-
-module.exports = function terminate(pid, callback) {
+module.exports = function terminate(pid) {
   if(!pid) {
     throw new Error("No pid supplied to Terminate!")
   }
+
+  // extract optional arguments.  Any can be omitted, but the
+  // ones present have to be in the
+  var i = 1;
+  var signal, options, callback;
+  if (typeof arguments[i] === 'string') {
+    signal = arguments[i++];
+  }
+  if (typeof arguments[i] === 'object' && arguments[i]) {
+    options = arguments[i++];
+  }
+  if (typeof arguments[i] === 'function') {
+    callback = arguments[i++];
+  }
+
   psTree(pid, function (err, children) {
-    handlePsTreeCallback(err, children, pid, callback);
+    handlePsTreeCallback(
+      err,
+      children,
+      pid,
+      signal,
+      options,
+      callback
+    );
   });
 };
+
--- test/test.js
+++ test/test.js
@@ -7,12 +7,40 @@ var exec = require('child_process').exec;
 var terminate = require('../terminate');
 var handlePsTreeCallback = require('../handlePsTreeCallback')
 
-test(cyan('Spawn a Parent process which has a few Child Processes'), function (t) {
-  var parent = exec("node ./test/exec/parent.js", function(error, stdout, stderr) {
-    // console.log('stdout: ' + stdout);
-    // console.log('stderr: ' + stderr);
-    if (error !== null) {
-        console.log('exec error: ' + error);
+test(cyan('kills all processes'), function (t) {
+  var parent = exec("node " + require.resolve('./exec/parent.js'), function(error, stdout, stderr) {
+    if (error) {
+      // this happens even during successful testing because the parent gets SIGKILLed
+      console.log('exec error: ' + error);
+    }
+  })
+  setTimeout(function() {
+    console.log(cyan("Parent:" + parent.pid));
+    // list all the child processes
+    psTree(parent.pid, function (err, children) {
+      // console.log(red(err));
+      console.log(cyan("Children: "), children)
+      t.true(children.length > 0, green("✓ There are " + children.length + " active child processes"));
+      setTimeout(function () {
+        terminate(parent.pid, function (err) {
+          t.equal(err, null, green("✓ No Errors"))
+        })
+        setTimeout(function () {
+          psTree(parent.pid, function (err, children) {
+            // console.log("Children: ", children, '\n');
+            t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
+            t.end();
+          })
+        }, 200); // give psTree time to kill the processes
+      }, 10); // give the child process time to spawn
+    });
+  }, 200); // give the child process time to spawn
+});
+
+test(cyan('works with custom pollInterval'), function (t) {
+  var parent = exec("node " + require.resolve('./exec/parent.js'), function(error, stdout, stderr) {
+    if (error) {
+      console.log('exec error: ' + error);
     }
   })
   setTimeout(function() {
@@ -21,21 +49,115 @@ test(cyan('Spawn a Parent process which has a few Child Processes'), function (t
     psTree(parent.pid, function(err, children) {
       // console.log(red(err));
       console.log(cyan("Children: "), children)
-      t.true(children.length > 0, green("✓ There are "+children.length+" active child processes"));
-    })
-    setTimeout(function() {
-      terminate(parent.pid, function(err, done) {
-        t.equal(err, null, green("✓ No Errors"))
-        t.equal(done, true, green("✓ Terminator killed all the processes"))
-      })
-      setTimeout(function() {
-        psTree(parent.pid, function (err, children) {
-          // console.log("Children: ", children, '\n');
-          t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
-          t.end();
+      t.true(children.length > 0, green("✓ There are " + children.length + " active child processes"));
+      setTimeout(function () {
+        terminate(parent.pid, {pollInterval: 1000}, function (err) {
+          t.equal(err, null, green("✓ No Errors"))
         })
-      },200); // give psTree time to kill the processes
-    },10); // give the child process time to spawn
+        setTimeout(function () {
+          psTree(parent.pid, function (err, children) {
+            // console.log("Children: ", children, '\n');
+            t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
+            t.end();
+          })
+        }, 200); // give psTree time to kill the processes
+      }, 10); // give the child process time to spawn
+    });
+  },200); // give the child process time to spawn
+});
+
+test(cyan('works when process takes awhile to exit'), function (t) {
+  var parent = exec(
+    "node " + require.resolve('./exec/parent.js'),
+    {env: {KILL_DELAY: '1000'}},
+    function(error, stdout, stderr) {
+      if (error) {
+        console.log('exec error: ' + error);
+      }
+    }
+  )
+  setTimeout(function() {
+    console.log(cyan("Parent:"+parent.pid));
+    // list all the child processes
+    psTree(parent.pid, function(err, children) {
+      // console.log(red(err));
+      console.log(cyan("Children: "), children)
+      t.true(children.length > 0, green("✓ There are " + children.length + " active child processes"));
+      setTimeout(function () {
+        terminate(parent.pid, function (err) {
+          t.equal(err, null, green("✓ No Errors"))
+        })
+        setTimeout(function () {
+          psTree(parent.pid, function (err, children) {
+            // console.log("Children: ", children, '\n');
+            t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
+            t.end();
+          })
+        }, 1500); // give psTree time to kill the processes
+      }, 10); // give the child process time to spawn
+    });
+  },200); // give the child process time to spawn
+});
+
+test(cyan('errors when process takes too long to exit'), function (t) {
+  var parent = exec(
+    "node " + require.resolve('./exec/parent.js'),
+    {env: {KILL_DELAY: '2000'}},
+    function(error, stdout, stderr) {
+      if (error) {
+        console.log('exec error: ' + error);
+      }
+    }
+  )
+  setTimeout(function() {
+    console.log(cyan("Parent:"+parent.pid));
+    // list all the child processes
+    psTree(parent.pid, function(err, children) {
+      // console.log(red(err));
+      console.log(cyan("Children: "), children)
+      t.true(children.length > 0, green("✓ There are " + children.length + " active child processes"));
+      setTimeout(function () {
+        terminate(parent.pid, 'SIGINT', {timeout: 1000}, function (err) {
+          t.assert(err && /^timed out waiting for pids \d+(, \d+)* to exit$/.test(err.message), green("✓ got expected error message"));
+        })
+        setTimeout(function () {
+          psTree(parent.pid, function (err, children) {
+            // console.log("Children: ", children, '\n');
+            t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
+            t.end();
+          })
+        }, 1500); // give psTree time to kill the processes
+      }, 10); // give the child process time to spawn
+    });
+  },200); // give the child process time to spawn
+});
+
+test(cyan('sends signal user passed'), function (t) {
+  var parent = exec("node " + require.resolve('./exec/parent.js'), function(error, stdout, stderr) {
+    t.assert(/^parent got SIGINT$/m.test(stdout), green("✓ parent reported getting SIGINT"));
+    if (error) {
+      console.log('exec error: ' + error);
+    }
+  })
+  setTimeout(function() {
+    console.log(cyan("Parent:"+parent.pid));
+    // list all the child processes
+    psTree(parent.pid, function(err, children) {
+      console.log(cyan("Children: "), children)
+      t.true(children.length > 0, green("✓ There are " + children.length + " active child processes"));
+      setTimeout(function () {
+        terminate(parent.pid, 'SIGINT', function (err) {
+          t.equal(err, null, green("✓ No Errors"))
+        })
+        setTimeout(function () {
+          psTree(parent.pid, function (err, children) {
+            // console.log("Children: ", children, '\n');
+            t.equal(children.length, 0, green("✓ No more active child processes (we killed them)"));
+            t.end();
+          })
+        }, 200); // give psTree time to kill the processes
+      }, 10); // give the child process time to spawn
+    });
   },200); // give the child process time to spawn
 });
 
@@ -51,7 +173,7 @@ test(cyan('Attempt to terminate without providing a Parent Process ID'), functio
 });
 
 test(cyan('Terminate a process without providing a callback'), function (t) {
-  var child = exec("node ./test/exec/child.js", function(error, stdout, stderr) { });
+  var child = exec("node " + require.resolve('./exec/parent.js'), function(error, stdout, stderr) { });
   setTimeout(function(){
     terminate(child.pid);
     setTimeout(function() {
@@ -66,10 +188,30 @@ test(cyan('Terminate a process without providing a callback'), function (t) {
   },200); // give the child process time to spawn
 });
 
+test(cyan("errors if parent pid doesn't exist"), function (t) {
+  var pid = 9000;
+  function exists() {
+    try {
+      process.kill(pid, 0);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+  while (exists()) {
+    pid++;
+  }
+
+  terminate(pid, function(err) {
+    t.assert(err, green("✓ got error"));
+    t.end();
+  });
+});
+
 test(cyan('immediately passes along errors from psTree'), function (t) {
   var error = new Error('test');
-  handlePsTreeCallback(error, undefined, 1, function (err) {
-    t.equal(error, err);
+  handlePsTreeCallback(error, undefined, 1, undefined, undefined, function (err) {
+    t.equal(error, err, green("✓ got expected error"));
     t.end();
   });
 });
